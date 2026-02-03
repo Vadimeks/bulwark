0c7cd3f11a72421b73baa147c0b068bb396dd283
Vadzim Simanau <eks220885@gmail.com>
2026-02-04 00:09:38 +0200

    add new sections,  mod json lang, add swipers

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import "./css/main.css";
 import { initHeader } from "./js/header.js";
 import { initLanguageSwitcher } from "./js/i18n.js";
+// 1. Імпартуем ініцыялізацыю слайдэраў
+import { initMaterialsSliders } from "./js/materials.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initHeader();
   initLanguageSwitcher();
+  // 2. Выклікаем функцыю для матэрыялаў
+  initMaterialsSliders();
 });
